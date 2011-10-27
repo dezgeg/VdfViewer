26e987a4583e33e9b7384a436991026250eb4c53
Tuomas Tynkkynen <dezgeg@gmail.com>
2011-10-27 22:43:31 +0300

    Improved the animation scripts, started writing documentation.

--- animate.sh
+++ animate.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 mkdir output/ 2>/dev/null
-FILEBASE=`basename $1 | sed 's/\(.*\)\..*/\1/'`
-OUTFILE=`echo output/out.$FILEBASE.txt`
-./planets $1 $OUTFILE || exit 1
-./plot.m $OUTFILE $FILEBASE
-convert -delay 20 -loop 0 output/$FILEBASE-frame*.gif output/$FILEBASE.gif
-rm -rf output/$FILEBASE-frame*.gif $OUTFILE
-echo Generating output/$FILEBASE.gif and output/$FILEBASE.png complete
+FILEBASE=`basename "$1" 2>/dev/null | sed -e 's/\(.*\)\..*/\1/'`
+OUTFILE=`echo "output/$FILEBASE.out"`
+rm -rf "$OUTFILE"
+./planets "$1" "$OUTFILE" || exit 1
+./plot.m "$OUTFILE" "$FILEBASE" "$2"
+if [ ! "$2" = "-n" ]
+then
+	echo -n "Generating output/$FILEBASE.gif... "
+	convert -delay 20 -loop 0 "output/$FILEBASE-frame"*.gif "output/$FILEBASE.gif"
+	rm -rf "output/$FILEBASE-frame"*.gif
+	echo "complete"
+fi
--- plot.m
+++ plot.m
@@ -1,25 +1,29 @@
 #!/usr/bin/octave -qf
-set(0, 'defaultfigurevisible', 'off');
+set(0, 'defaultfigurevisible', 'off'); % hide plot window
 args = argv();
-data = load(args{1});
+data = load(args{1}); % open input file
+noanim = strcmp(args{3}, '-n');
 n = data(1, 1);
 grid on;
-hold on
+hold on;
 
 for i = 0:(n-1)
 		xs = data(:, 3 + 3*i);
 		ys = data(:, 4 + 3*i);
 		zs = data(:, 5 + 3*i);
 
-		plot3(xs, ys, zs, sprintf('o-%d', i+1))
+		plot3(xs, ys, zs, sprintf('o-%d', i+1));
 end
 print(sprintf('output/%s.png', args{2}))
+if noanim
+	exit();
+end
 a = axis;
 clf
 m = rows(data);
 
 for j = 1:m
-		hold on
+		hold on;
 		for i = 0:(n-1)
 			xs = [data(1:j, 3 + 3*i)];
 			ys = [data(1:j, 4 + 3*i)];
@@ -33,8 +37,8 @@ for j = 1:m
 				plot3(xs, ys, zs, sprintf('-*%d', i+1));
 			end
 		end
-		hold off
+		hold off;
 		print(sprintf('output/%s-frame%05d.gif', args{2}, j))
-		clf
+		clf;
 		j
 end
